70fe57c9140c52537b2fc46f1e97e11a0fa323a8
Kuandikov Askar <askarkuandikov31.05@gmail.com>
2022-12-15 02:03:02 +0600

    added search

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -19,6 +19,7 @@ class EmployeesAddForm extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.name.length < 3 || !this.state.salary) return;
     this.props.onAdd(this.state.name, this.state.salary);
     this.setState({
       name: "",
@@ -32,7 +33,7 @@ class EmployeesAddForm extends Component {
     return (
       <div className="app-add-form">
         <h3>Добавьте сотрудника</h3>
-        <form action="" className="add-form d-flex">
+        <form action="" className="add-form d-flex" onSubmit={this.onSubmit}>
           <input
             type="text"
             className="form-control new-post-label"
