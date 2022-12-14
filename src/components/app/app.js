93b0511ffb6d5688ec7ff4b54052d264e1da43af
Kuandikov Askar <askarkuandikov31.05@gmail.com>
2022-12-15 01:14:09 +0600

    modified onToggle function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -62,22 +62,11 @@ class App extends Component {
     });
   };
 
-  onToggleIncrease = (id) => {
+  onToggleProp = (id, prop) => {
     this.setState(({ data }) => ({
       data: data.map((item) => {
         if (item.id === id) {
-          return { ...item, increase: !item.increase };
-        }
-        return item;
-      }),
-    }));
-  };
-
-  onToggleRise = (id) => {
-    this.setState(({ data }) => ({
-      data: data.map((item) => {
-        if (item.id === id) {
-          return { ...item, rise: !item.rise };
+          return { ...item, [prop]: !item[prop] };
         }
         return item;
       }),
@@ -100,8 +89,7 @@ class App extends Component {
         <EmployeesList
           data={this.state.data}
           onDelete={this.deleteItem}
-          onToggleIncrease={this.onToggleIncrease}
-          onToggleRise={this.onToggleRise}
+          onToggleProp={this.onToggleProp}
         />
         <EmployeesAddForm onAdd={this.addItem} />
       </div>
